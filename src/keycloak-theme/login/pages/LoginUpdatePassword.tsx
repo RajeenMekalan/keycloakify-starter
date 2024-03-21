c984a097503ea83ea8913f6a2d464f8e3918a6f4
Rajeenthini Rajaratnam <Rajeenthini@cemexhrm.com>
2024-03-21 17:59:24 +0530

    Update Password

--- src/keycloak-theme/login/pages/LoginUpdatePassword.tsx
+++ src/keycloak-theme/login/pages/LoginUpdatePassword.tsx
@@ -5,8 +5,8 @@ import type { KcContext } from "../kcContext";
 import type { I18n } from "../i18n";
 import logo from "../assets/logo.png"
 import random from "../assets/Random.svg"
-import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
+// import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+// import { faEye, faEyeSlash } from '@fortawesome/free-solid-svg-icons';
 import { useState } from "react";
 import eyeicon from "../assets/eyeIcon.svg";
 import eyeiconInvisible from "../assets/eyeIconInvisible.svg";
@@ -19,7 +19,7 @@ export default function LoginUpdatePassword(props: PageProps<Extract<KcContext, 
         classes
     });
 
-    const { url, realm, auth } = kcContext;
+    const { url, realm, auth , username} = kcContext;
 
     const { msg, msgStr } = i18n;
     
@@ -82,95 +82,179 @@ export default function LoginUpdatePassword(props: PageProps<Extract<KcContext, 
 
 
     return (
-        <Template {...{ kcContext, i18n, doUseDefaultCss, classes }} headerNode={msg("updatePasswordTitle")}>
-             <div>
-                <img src={logo} style={{ width: '150px', height: '30px', marginRight: '10px', marginBottom: '20px' }} />
-                <div style={{ fontWeight: 400, fontSize: '25px', lineHeight: '40px', color: '#253053', textAlign: 'left', marginBottom: '20px' }}>
-                    Set Your Password
-                </div>
-            </div>
+      <Template
+        {...{ kcContext, i18n, doUseDefaultCss, classes }}
+        headerNode={msg("updatePasswordTitle")}
+      >
+        <div>
+          <img
+            src={logo}
+            style={{
+              width: "150px",
+              height: "30px",
+              marginRight: "10px",
+              marginBottom: "20px",
+            }}
+          />
+          <div
+            style={{
+              fontWeight: 400,
+              fontSize: "25px",
+              lineHeight: "40px",
+              color: "#253053",
+              textAlign: "left",
+              marginBottom: "20px",
+            }}
+          >
+            Set Your Password
+          </div>
+        </div>
 
-            <form id="kc-passwd-update-form" className={getClassName("kcFormClass")} action={url.loginAction} method="post">                
-            <div className={getClassName("kcFormGroupClass")}>                                     
-                    <div className="floating-label-group">
-                        <input
-                            type={isPasswordVisible ? "text" : "password"}
-                            id="newPassowrd"
-                            name="newPassowrd"
-                            className={getClassName("kcInputClass") + " form-control"}
-                            onInvalid={(e) => handleInvalidInput(e, 'Enter a Password')}
-                            onChange={handleBlur}
-                            required
-                            
-                        />
-                        <label htmlFor="newPassowrd" className={getClassName("kcLabelClass") + " floating-label"} >
-                            New Password
-                        </label>
-                        <img
-                            src={isPasswordVisible ? eyeicon : eyeiconInvisible}
-                            alt="Toggle password visibility"
-                            className="password-toggle-icon"
-                            onClick={togglePasswordVisibility}
-                            style={{ cursor: 'pointer', position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }}
-                        />
-                    </div>                  
-                </div>
-                <div className={getClassName("kcFormGroupClass")} style={{marginTop:'20px'}}>                                     
-                    <div className="floating-label-group">
-                        <input
-                            type={isConfirmPasswordVisible ? "text" : "password"}
-                            id="confirmPassowrd"
-                            name="confirmPassowrd"
-                            className={getClassName("kcInputClass") + " form-control"}  
-                            value={confirmPassword}
-                            onChange={handleConfirmPasswordChange} 
-                            onInvalid={(e) => handleInvalidInput(e, 'Confirm your Password')}
-                            required                         
-                        />
-                        <label htmlFor="confirmPassowrd" className={getClassName("kcLabelClass") + " floating-label"} >
-                            Confirm Password
-                        </label>
-                        <img
-                            src={isConfirmPasswordVisible ? eyeicon : eyeiconInvisible}
-                            alt="Toggle password visibility"
-                            className="password-toggle-icon"
-                            onClick={toggleConfirmPasswordVisibility}
-                            style={{ cursor: 'pointer', position: 'absolute', right: '10px', top: '50%', transform: 'translateY(-50%)' }}
-                        />
-                    </div>                  
-                </div>              
-                <div id="kc-form-buttons">
-                    <input
-                        className={clsx(
-                            getClassName("kcButtonClass"),
-                            // getClassName("kcButtonPrimaryClass"),
-                            getClassName("kcButtonBlockClass"),
-                            getClassName("kcButtonLargeClass")
-                        )}
-                        type="submit"
-                        value="Reset Password"
-                        style={{ backgroundColor: '#2C82F9', borderRadius: '6px', color: '#FFFFFF' }}
-                    />
-                </div>
-                <div className="separator" style={{ marginTop: '30px'}}>
-                    <span style={{ fontSize: '14px', color: '#8C8C8C' }}>or</span>
-                </div>
-                <div id="kc-form-buttons">
-                    <button
-                        className={clsx(
-                            getClassName("kcButtonClass"),
-                            getClassName("kcButtonPrimaryClass"),
-                            getClassName("kcButtonBlockClass"),
-                            getClassName("kcButtonLargeClass")
-                        )}
-                        type="submit"
-                        style={{ borderRadius: '6px', fontSize: '14px', border: '1px solid #1E24323B', background: '#FFFFFF', color: '#2C82F9', fontWeight: '400px', paddingRight: '0px' }}
-                    >
-                        <img src={random} style={{ width: '14px', height: '14px', marginRight: '5px', marginBottom:'2px'}} />
-                        Generate Password
-                    </button>
-                </div>
-            </form>
-        </Template>
+        <form
+          id="kc-passwd-update-form"
+          className={getClassName("kcFormClass")}
+          action={url.loginAction}
+          method="post"
+        >
+          <input
+            type="text"
+            id="username"
+            name="username"
+            value={username}
+            readOnly={true}
+            autoComplete="username"
+            style={{ display: "none" }}
+          />
+          <input
+            type="password"
+            id="password"
+            name="password"
+            autoComplete="current-password"
+            style={{ display: "none" }}
+          />
+
+          <div className={getClassName("kcFormGroupClass")}>
+            <div className="floating-label-group">
+              <input
+                type={isPasswordVisible ? "text" : "password"}
+                id="password-new"
+                name="password-new"
+                className={getClassName("kcInputClass") + " form-control"}
+                onInvalid={(e) => handleInvalidInput(e, "Enter a Password")}
+                onChange={handleBlur}
+                required
+              />
+              <label
+                htmlFor="password-new"
+                className={getClassName("kcLabelClass") + " floating-label"}
+              >
+                New Password
+              </label>
+              <img
+                src={isPasswordVisible ? eyeicon : eyeiconInvisible}
+                alt="Toggle password visibility"
+                className="password-toggle-icon"
+                onClick={togglePasswordVisibility}
+                style={{
+                  cursor: "pointer",
+                  position: "absolute",
+                  right: "10px",
+                  top: "50%",
+                  transform: "translateY(-50%)",
+                }}
+              />
+            </div>
+          </div>
+          <div
+            className={getClassName("kcFormGroupClass")}
+            style={{ marginTop: "20px" }}
+          >
+            <div className="floating-label-group">
+              <input
+                type={isConfirmPasswordVisible ? "text" : "password"}
+                id="password-confirm"
+                name="password-confirm"
+                className={getClassName("kcInputClass") + " form-control"}
+                value={confirmPassword}
+                onChange={handleConfirmPasswordChange}
+                onInvalid={(e) =>
+                  handleInvalidInput(e, "Confirm your Password")
+                }
+                required
+              />
+              <label
+                htmlFor="password-confirm"
+                className={getClassName("kcLabelClass") + " floating-label"}
+              >
+                Confirm Password
+              </label>
+              <img
+                src={isConfirmPasswordVisible ? eyeicon : eyeiconInvisible}
+                alt="Toggle password visibility"
+                className="password-toggle-icon"
+                onClick={toggleConfirmPasswordVisibility}
+                style={{
+                  cursor: "pointer",
+                  position: "absolute",
+                  right: "10px",
+                  top: "50%",
+                  transform: "translateY(-50%)",
+                }}
+              />
+            </div>
+          </div>
+          <div id="kc-form-buttons">
+            <input
+              className={clsx(
+                getClassName("kcButtonClass"),
+                // getClassName("kcButtonPrimaryClass"),
+                getClassName("kcButtonBlockClass"),
+                getClassName("kcButtonLargeClass")
+              )}
+              type="submit"
+              value="Reset Password"
+              style={{
+                backgroundColor: "#2C82F9",
+                borderRadius: "6px",
+                color: "#FFFFFF",
+              }}
+            />
+          </div>
+          <div className="separator" style={{ marginTop: "30px" }}>
+            <span style={{ fontSize: "14px", color: "#8C8C8C" }}>or</span>
+          </div>
+          <div id="kc-form-buttons">
+            <button
+              className={clsx(
+                getClassName("kcButtonClass"),
+                getClassName("kcButtonPrimaryClass"),
+                getClassName("kcButtonBlockClass"),
+                getClassName("kcButtonLargeClass")
+              )}
+              type="submit"
+              style={{
+                borderRadius: "6px",
+                fontSize: "14px",
+                border: "1px solid #1E24323B",
+                background: "#FFFFFF",
+                color: "#2C82F9",
+                fontWeight: "400px",
+                paddingRight: "0px",
+              }}
+            >
+              <img
+                src={random}
+                style={{
+                  width: "14px",
+                  height: "14px",
+                  marginRight: "5px",
+                  marginBottom: "2px",
+                }}
+              />
+              Generate Password
+            </button>
+          </div>
+        </form>
+      </Template>
     );
 }
